a5c986af2d48dd7e30fe2ba67f99a2131ea6bb5b
Rami Shook <ramishook99@gmail.com>
2022-05-01 14:07:57 +0300

    remove commented code

--- app.js
+++ app.js
@@ -1,34 +1,8 @@
-// const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const authRoutes = require('./routes/auth.route');
-// const refreshTokenRoutes = require('./routes/refreshToken.route');
-// const userRoutes = require('./routes/user.route');
-// const menuRoutes = require('./routes/menu.route');
-// const restaurantBranchRoutes = require('./routes/restaurantBranch.route');
-// const restaurantRoutes = require('./routes/restaurant.route');
-// const orderRoutes = require('./routes/order.route');
+
 const { createServer } = require('./utils/server');
-// require('dotenv').config();
 
 const app = createServer();
-// const corsOptions = {
-//   origin: '*',
-//   methods: 'GET, POST, DELETE, PUT',
-// };
-
-// const app = express();
-// app.use(cors(corsOptions));
-// app.use(bodyParser.json());
-
-// app.use('/api', authRoutes);
-// app.use('/api/refreshToken', refreshTokenRoutes);
-// app.use('/api', userRoutes);
-// app.use('/api', menuRoutes);
-// app.use('/api', restaurantBranchRoutes);
-// app.use('/api', restaurantRoutes);
-// app.use('/api', orderRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URL, {
